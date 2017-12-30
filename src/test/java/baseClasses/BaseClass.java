606409ceb1826cf6c986b9c1cc05938aad2b3338
Jemal <jemal@zoomdata.com>
2017-12-30 16:26:12 -0500

    modify maximize driver

--- src/test/java/baseClasses/BaseClass.java
+++ src/test/java/baseClasses/BaseClass.java
@@ -16,7 +16,7 @@ public class BaseClass extends BrowserFactory {
 
     public void goTo() {
         driver.get(url);
-       driver.manage().window().setSize(new Dimension(1600,900));
+       //driver.manage().window().setSize(new Dimension(1600,900));
     }
 
     @AfterClass
